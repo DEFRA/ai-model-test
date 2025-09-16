b4dbd7de476733ee492ae0ab4dce4b45e0e64a1a
sfitz42 <87677173+sfitz42@users.noreply.github.com>
2025-09-16 11:59:41 +0100

    Use correct region prop

--- app/utils/langchain_bedrock_client.py
+++ app/utils/langchain_bedrock_client.py
@@ -7,7 +7,7 @@ def chat_bedrock_client():
     model = settings.AWS_BEDROCK_MODEL
 
     llm = ChatBedrockConverse(
-            region=settings.aws_region,
+            region_name=settings.aws_region,
             model_id=model,
             provider="anthropic",
             guardrails={"guardrailIdentifier": settings.AWS_BEDROCK_GUARDRAIL, "guardrailVersion": settings.AWS_BEDROCK_GUARDRAIL_VERSION, "trace": "enabled"}
