312285ff75a00b1cd54f8f1fc672ed3eaa838d78
sfitz42 <87677173+sfitz42@users.noreply.github.com>
2025-09-16 11:43:11 +0100

    Add llm provider

--- app/utils/langchain_bedrock_client.py
+++ app/utils/langchain_bedrock_client.py
@@ -9,6 +9,7 @@ def chat_bedrock_client():
     llm = ChatBedrockConverse(
             region=settings.aws_region,
             model_id=model,
+            provider="anthropic",
             guardrails={"guardrailId": settings.AWS_BEDROCK_GUARDRAIL, "guardrailVersion": settings.AWS_BEDROCK_GUARDRAIL_VERSION, "trace": "enabled"}
     )
 
