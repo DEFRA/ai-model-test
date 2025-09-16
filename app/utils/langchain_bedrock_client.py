0fe740018058a0685b76109d8fb58d56015c7d3f
sfitz42 <87677173+sfitz42@users.noreply.github.com>
2025-09-16 09:58:05 +0100

    Upgrade langchain

--- app/utils/langchain_bedrock_client.py
+++ app/utils/langchain_bedrock_client.py
@@ -1,4 +1,4 @@
-from langchain_aws import ChatBedrock
+from langchain_aws import ChatBedrockConverse
 
 from app.config import config as settings
 
@@ -6,10 +6,9 @@ from app.config import config as settings
 def chat_bedrock_client():
     model = settings.AWS_BEDROCK_MODEL
 
-    llm = ChatBedrock(
+    llm = ChatBedrockConverse(
             region=settings.aws_region,
             model=model,
-            beta_use_converse_api=True,
             guardrails={"guardrailId": settings.AWS_BEDROCK_GUARDRAIL, "guardrailVersion": settings.AWS_BEDROCK_GUARDRAIL_VERSION, "trace": "enabled"}
     )
 
